d4114ab16ef3683e84905b5d6baaae0e641565b9
David Gonzalez <venezuelavvv01@protonmail.com>
2022-11-06 14:47:22 -0500

    Imprimir datos comunes  int, long, double, boolean, String.
     imprimir todos los datos que se han visto en las sesiones.
    
    Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.

---
+++ src/OpenBootcam/JavaBasico.java
@@ -0,0 +1,14 @@
+package OpenBootcam;
+
+public class JavaBasico {
+    public static void main(String[] args) {
+        int numero1 = 12;
+        long numero2 = 45696966;
+        double numero3 = 52.23d;
+        boolean bootcampBueno = true;
+        boolean bootcampMalo = false;
+        String f = "Este es el mejor bootcamp";
+        System.out.println( numero1 +" " + numero2+" "+ numero3+" "+bootcampMalo+" "+bootcampBueno+ " "+f);
+
+    }
+}
